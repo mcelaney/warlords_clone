5337dfb8990cb551c6788a4a8f5e24fbbc5b3b58
Brian E. McElaney <brian.e.mcelaney@gmail.com>
2023-12-18 20:42:25 -0500

    Improve testing for Battle.perform

--- warlords/lib/warlords/core/battle.ex
+++ warlords/lib/warlords/core/battle.ex
@@ -4,7 +4,6 @@ defmodule Warlords.Core.Battle do
   """
 
   alias Warlords.Core.Battle.Formation
-  alias Warlords.Core.Gameboard.Tile
 
   @doc """
   Organizes and performs a battle between an attacking tile and defending tiles
@@ -13,33 +12,67 @@ defmodule Warlords.Core.Battle do
   the battle as well as a list of any surviving units.
 
   The battle will always result in a winner. Ties are rerolled until broken.
+
+  This function is set up this way to allow us to isolate the die roll from d12/1
+  for testing as it returns a random number between 1 and 12.
+
+  Function defaults are for testing purposes and should not be used in production
+  as any functions passed in will only be leveraged on the first step of a battle.
+  All subsequent steps will use the default functions.
+
   """
-  @spec perform(Tile.t(), [Tile.t()], Tile.t(), (-> integer()), module) ::
-          {winner :: :attackers | :defenders, [Formation.t()]}
-  def perform(attacker_tile, defender_tiles, target_tile, d12_fn \\ &d12/0, template) do
-    resolve_battle(
-      Formation.attack_formation(attacker_tile, target_tile, template),
-      Formation.defense_formation(defender_tiles, target_tile, template),
-      d12_fn
-    )
+  @spec perform([Formation.t()], [Formation.t()]) :: {:attacker | :defender, [Formation.t()]}
+  @spec perform([Formation.t()], [Formation.t()], fun, fun, fun) ::
+          {:attacker | :defender, [Formation.t()]}
+  def perform(
+        attackers,
+        defenders,
+        pref_off_fn \\ &next_unit_attack/1,
+        prep_def_fn \\ &next_unit_attack/1,
+        next_step_fn \\ &next_step/3
+      )
+
+  def perform(attackers, defenders, pref_off_fn, prep_def_fn, next_step_fn) do
+    with {attack_strength, offense} <- pref_off_fn.(attackers),
+         {defense_strength, defense} <- prep_def_fn.(defenders),
+         result <- find_winner(attack_strength, defense_strength) do
+      next_step_fn.(result, offense, defense)
+    end
   end
 
-  defp resolve_battle([], defenders, _), do: {:defenders, defenders}
-  defp resolve_battle(attackers, [], _), do: {:attackers, attackers}
+  @spec next_unit_attack([Formation.t()], (-> integer())) :: {integer, [Formation.t()]}
+  def next_unit_attack([head | _tail] = units, d12_fn \\ &d12/0),
+    do: {head.strength + d12_fn.(), units}
 
-  defp resolve_battle([attacker | attackers] = offense, [defender | defenders] = defense, d12_fn) do
-    attack_val = attacker.strength + d12_fn.()
-    defense_val = defender.strength + d12_fn.()
+  @spec next_step(atom, [Formation.t()], [Formation.t()], fun) ::
+          {:attacker | :defender, [Formation.t()]}
+  def next_step(
+        result,
+        [_ | remaining_attackers] = attackers,
+        [_ | remaining_defenders] = defenders,
+        continue_fn \\ &continue/1
+      ) do
+    result
+    |> case do
+      :attacker_wins -> {attackers, remaining_defenders}
+      :defender_wins -> {remaining_attackers, defenders}
+      :tie -> {attackers, defenders}
+    end
+    |> continue_fn.()
+  end
 
+  @spec continue({[Formation.t()], [Formation.t()]}, fun) ::
+          {:attacker_wins | :defender_wins, [Formation.t()]}
+  def continue(units, perform_fn \\ &perform/2)
+  def continue({[], defenders}, _), do: {:defender_wins, defenders}
+  def continue({attackers, []}, _), do: {:attacker_wins, attackers}
+  def continue({attackers, defenders}, perform_fn), do: perform_fn.(attackers, defenders)
+
+  defp find_winner(attack_val, defense_val) do
     cond do
-      attack_val > defense_val ->
-        resolve_battle(offense, defenders, d12_fn)
-
-      attack_val < defense_val ->
-        resolve_battle(attackers, defense, d12_fn)
-
-      true ->
-        resolve_battle(offense, defense, d12_fn)
+      attack_val > defense_val -> :attacker_wins
+      attack_val < defense_val -> :defender_wins
+      true -> :tie
     end
   end
 
--- warlords/test/warlords/core/battle_test.exs
+++ warlords/test/warlords/core/battle_test.exs
@@ -1,160 +1,152 @@
 defmodule Warlords.Core.BattleTest do
   use ExUnit.Case
 
-  import Warlords.GameboardFixtures
-  import Warlords.UnitFixtures
   alias Warlords.Core.Battle
-  alias Warlords.Mock.RandomNumber
-  alias Warlords.Mock.Template
+  alias Warlords.Core.Battle.Formation
 
-  describe "perform/5" do
-    test "attacker wins" do
-      attacker_units = standard_fixture(%{strength: 5})
-
-      attacker =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :elvallie, units: [attacker_units]})})
-
-      target_units = standard_fixture(%{strength: 5})
-
-      target =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :storm_giants, units: [target_units]})})
-
-      attacker_roll_1 = 5
-      defender_roll_1 = 4
-      RandomNumber.start_link([attacker_roll_1, defender_roll_1])
-
-      {winner, [result_unit | _] = remaining_units} =
-        Battle.perform(attacker, [target], target, &RandomNumber.pop/0, Template)
-
-      expected_number_of_turns = RandomNumber.is_empty()
-      RandomNumber.stop()
-
-      assert expected_number_of_turns
-      assert winner == :attackers
-      assert Enum.count(remaining_units) == 1
-      assert attacker_units == result_unit.unit
+  describe "perform/2" do
+    setup do
+      %{
+        attackers: [%Formation{rank: 1, strength: 4, origin: {3, 3}, unit: "A"}],
+        defenders: [%Formation{rank: 1, strength: 4, origin: {3, 3}, unit: "D"}],
+        next_step_fn: fn result, offense, defense -> {result, offense, defense} end
+      }
     end
 
-    test "attacker defeats multiple defenders" do
-      attacker_units = standard_fixture(%{strength: 5})
+    test "Attacker wins", %{
+      attackers: attackers,
+      defenders: defenders,
+      next_step_fn: next_step_fn
+    } do
+      pref_off_fn = fn units -> {6, units} end
+      pref_def_fn = fn units -> {5, units} end
 
-      attacker =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :elvallie, units: [attacker_units]})})
+      {result, offense, defense} =
+        Battle.perform(attackers, defenders, pref_off_fn, pref_def_fn, next_step_fn)
 
-      target_units = [standard_fixture(%{strength: 5}), standard_fixture(%{strength: 5})]
-
-      target =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :storm_giants, units: target_units})})
-
-      attacker_roll_1 = 5
-      defender_roll_1 = 4
-      attacker_roll_2 = 5
-      defender_roll_2 = 4
-
-      RandomNumber.start_link([attacker_roll_1, defender_roll_1, attacker_roll_2, defender_roll_2])
-
-      {winner, remaining_units} =
-        Battle.perform(attacker, [target], target, &RandomNumber.pop/0, Template)
-
-      expected_number_of_turns = RandomNumber.is_empty()
-      RandomNumber.stop()
-
-      assert expected_number_of_turns
-      assert winner == :attackers
-      assert Enum.count(remaining_units) == 1
+      assert result == :attacker_wins
+      assert offense == attackers
+      assert defense == defenders
     end
 
-    test "attacker loses a unit but wins multiple defenders" do
-      attacker_1 = standard_fixture(%{strength: 5, index: 1})
-      attacker_2 = standard_fixture(%{strength: 5, index: 2})
+    test "Defender wins", %{
+      attackers: attackers,
+      defenders: defenders,
+      next_step_fn: next_step_fn
+    } do
+      pref_off_fn = fn units -> {5, units} end
+      pref_def_fn = fn units -> {6, units} end
 
-      attacker =
-        tile_fixture(%{
-          stack: stack_fixture(%{empire_key: :elvallie, units: [attacker_1, attacker_2]})
-        })
+      {result, offense, defense} =
+        Battle.perform(attackers, defenders, pref_off_fn, pref_def_fn, next_step_fn)
 
-      target_units = [standard_fixture(%{strength: 5})]
-
-      target =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :storm_giants, units: target_units})})
-
-      # attack loses first round
-      attacker_roll_1 = 4
-      defender_roll_1 = 5
-      # attack wins second round
-      attacker_roll_2 = 5
-      defender_roll_2 = 4
-
-      RandomNumber.start_link([attacker_roll_1, defender_roll_1, attacker_roll_2, defender_roll_2])
-
-      {winner, [result_unit | _] = remaining_units} =
-        Battle.perform(attacker, [target], target, &RandomNumber.pop/0, Template)
-
-      expected_number_of_turns = RandomNumber.is_empty()
-      RandomNumber.stop()
-
-      assert expected_number_of_turns
-      assert winner == :attackers
-      assert Enum.count(remaining_units) == 1
-      # attacker 1 eliminated in the first round
-      # units attack in ascending index order
-      assert attacker_2 == result_unit.unit
+      assert result == :defender_wins
+      assert offense == attackers
+      assert defense == defenders
     end
 
-    test "ties reroll" do
-      attacker_units = standard_fixture(%{strength: 5})
+    test "tie", %{attackers: attackers, defenders: defenders, next_step_fn: next_step_fn} do
+      pref_off_fn = fn units -> {5, units} end
+      pref_def_fn = fn units -> {5, units} end
 
-      attacker =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :elvallie, units: [attacker_units]})})
+      {result, offense, defense} =
+        Battle.perform(attackers, defenders, pref_off_fn, pref_def_fn, next_step_fn)
 
-      target_units = [standard_fixture(%{strength: 5})]
-
-      target =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :storm_giants, units: target_units})})
-
-      attacker_roll_1 = 4
-      defender_roll_1 = 4
-      attacker_roll_2 = 5
-      defender_roll_2 = 4
-
-      RandomNumber.start_link([attacker_roll_1, defender_roll_1, attacker_roll_2, defender_roll_2])
-
-      {winner, remaining_units} =
-        Battle.perform(attacker, [target], target, &RandomNumber.pop/0, Template)
-
-      expected_number_of_turns = RandomNumber.is_empty()
-      RandomNumber.stop()
-
-      assert expected_number_of_turns
-      assert winner == :attackers
-      assert Enum.count(remaining_units) == 1
+      assert result == :tie
+      assert offense == attackers
+      assert defense == defenders
     end
 
-    test "defender wins" do
-      attacker_units = standard_fixture(%{strength: 5})
+    test "can be called with defaults", %{attackers: attackers, defenders: defenders} do
+      {result, units} = Battle.perform(attackers, defenders)
 
-      attacker =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :elvallie, units: [attacker_units]})})
+      assert result == :attacker_wins || result == :defender_wins
 
-      target_units = standard_fixture(%{strength: 5})
+      case result do
+        :defender_wins -> assert units == defenders
+        :attacker_wins -> assert units == attackers
+      end
+    end
+  end
 
-      target =
-        tile_fixture(%{stack: stack_fixture(%{empire_key: :storm_giants, units: [target_units]})})
+  describe "next_unit_attack/2" do
+    test "tags the stack with the final strength of next unit without reordering" do
+      stack = [
+        %Formation{rank: 3, strength: 8, origin: {3, 3}, unit: "A"},
+        %Formation{rank: 1, strength: 1, origin: {3, 3}, unit: "B"},
+        %Formation{rank: 2, strength: 2, origin: {3, 3}, unit: "C"}
+      ]
 
-      attacker_roll_1 = 4
-      defender_roll_1 = 5
-      RandomNumber.start_link([attacker_roll_1, defender_roll_1])
+      die_roll = fn -> 12 end
+      assert Battle.next_unit_attack(stack, die_roll) == {20, stack}
+    end
+  end
 
-      {winner, [result_unit | _] = remaining_units} =
-        Battle.perform(attacker, [target], target, &RandomNumber.pop/0, Template)
+  describe "next_step/4" do
+    setup do
+      %{
+        attackers: [
+          %Formation{rank: 1, strength: 1, origin: {3, 3}, unit: "A"},
+          %Formation{rank: 2, strength: 2, origin: {4, 3}, unit: "A"}
+        ],
+        defenders: [
+          %Formation{rank: 1, strength: 1, origin: {3, 4}, unit: "D"},
+          %Formation{rank: 2, strength: 2, origin: {4, 4}, unit: "D"}
+        ],
+        continue_fn: fn units -> units end
+      }
+    end
 
-      expected_number_of_turns = RandomNumber.is_empty()
-      RandomNumber.stop()
+    test "Reduces the losing side", %{
+      attackers: [_ | attacker_tail] = attackers,
+      defenders: [_ | defender_tail] = defenders,
+      continue_fn: continue_fn
+    } do
+      assert Battle.next_step(:attacker_wins, attackers, defenders, continue_fn) ==
+               {attackers, defender_tail}
 
-      assert expected_number_of_turns
-      assert winner == :defenders
-      assert Enum.count(remaining_units) == 1
-      assert target_units == result_unit.unit
+      assert Battle.next_step(:defender_wins, attackers, defenders, continue_fn) ==
+               {attacker_tail, defenders}
+    end
+
+    test "Does not reduce a tie", %{
+      attackers: attackers,
+      defenders: defenders,
+      continue_fn: continue_fn
+    } do
+      assert Battle.next_step(:tie, attackers, defenders, continue_fn) == {attackers, defenders}
+    end
+  end
+
+  describe "continue/2" do
+    setup do
+      %{
+        attackers: [
+          %Formation{rank: 1, strength: 1, origin: {3, 3}, unit: "A"},
+          %Formation{rank: 2, strength: 2, origin: {4, 3}, unit: "A"}
+        ],
+        defenders: [
+          %Formation{rank: 1, strength: 1, origin: {3, 4}, unit: "D"},
+          %Formation{rank: 2, strength: 2, origin: {4, 4}, unit: "D"}
+        ],
+        perform_fn: fn attackers, defenders -> {attackers, defenders} end
+      }
+    end
+
+    test "No defenders left", %{attackers: attackers} do
+      assert Battle.continue({attackers, []}, nil) == {:attacker_wins, attackers}
+    end
+
+    test "No attackers left", %{defenders: defenders} do
+      assert Battle.continue({[], defenders}, nil) == {:defender_wins, defenders}
+    end
+
+    test "Both sides have units left", %{
+      attackers: attackers,
+      defenders: defenders,
+      perform_fn: perform_fn
+    } do
+      assert Battle.continue({attackers, defenders}, perform_fn) == {attackers, defenders}
     end
   end
 end
